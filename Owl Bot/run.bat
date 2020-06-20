c0ea03fec7df57994d7ea3787e01f9daeeb0ae70
PhonieZ Dev <phoniezdev@yahoo.com>
2020-06-20 12:19:37 +0100

    Update run.bat

--- Owl Bot/run.bat
+++ Owl Bot/run.bat
@@ -1,2 +1,2 @@
-"C:\Users\phoni\AppData\Local\Programs\Python\Python38-32\python.exe" "C:\Users\phoni\Desktop\Filing Cabinet\Educational Stuff\Coding\Discord Stuff\Bots\Owl Bot\bot.py"
+"python here" "bot.py here"
 pause
